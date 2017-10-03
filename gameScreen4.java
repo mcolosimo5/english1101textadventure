2310ac24b1218bc1eb1ea4ab093c41a5fe4d6670
Michael Colosimo <michael@lawn-128-61-46-232.lawn.gatech.edu>
2017-10-02 22:06:53 -0400

    Working on level 2

---
+++ gameScreen4.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class gameScreen4 extends JFrame implements ActionListener {
+  
+  private static final long serialVersionUID = 1L;
+  
+  public static void main (String args [])
+  {
+    new gameScreen4().setVisible(true);
+  }
+  
+  public gameScreen4() {
+    super("Play Game");
+    setSize(800, 700);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setLayout(new FlowLayout());
+    
+    JLabel content = new JLabel("You must prepare to search for the killer. You go to the armory to get a weapon.");
+    add(content);
+    
+    JButton sword = new JButton("A Silver Sword");
+    sword.addActionListener(this);
+    add(sword);
+      
+    JButton bow = new JButton("A Bow and Arrow");
+    bow.addActionListener(this);
+    add(bow);
+    
+    JButton torch = new JButton("A Torch");
+    torch.addActionListener(this);
+    add(torch);
+    
+   }
+  
+   @Override 
+  public void actionPerformed (ActionEvent e) {
+    String a = e.getActionCommand();
+    
+    if (a.equals("A Silver Sword"))
+          {
+      new killedByVillagers().setVisible(true);
+    }
+    else if(a.equals("A Bow and Arrow"))
+      {
+      new gameScreen().setVisible(true);
+    }
+    else if(a.equals("A Torch"))
+      {
+      new gameScreen().setVisible(true);
+    }
+   }
+}
+  
+  
+      
+ 
+  
